1313adf6201a5e0fc1b2a80cad0f84d18fd71840
oshoemaker <oshoemaker@10.242.5.110>
2019-09-09 23:48:12 -0400

    project

---
+++ hello there class 1/src/ten.java
@@ -0,0 +1,20 @@
+
+public class ten {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("line 1 \n");
+		System.out.print("\tline 2 \n");
+		System.out.print("\tline 3 \n");
+		System.out.print("\tline 4 \n");
+		System.out.print("\tline 5 \n");
+		System.out.print("\tline 6 \n");
+		System.out.print("This class interest me \n there is so much you can do ");
+		System.out.println("line 8");
+		System.out.println("line 9 \n");
+		System.out.print("line 10 \n Even though this was a easy assignment \n \tI still had fun doing it");
+		
+
+	}
+
+}
