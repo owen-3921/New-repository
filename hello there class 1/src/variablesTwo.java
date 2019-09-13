3c540f7195bcddd282421720317d761e47668f13
oshoemaker <oshoemaker@10.242.5.110>
2019-09-12 23:49:41 -0400

    final project

--- hello there class 1/src/variablesTwo.java
+++ hello there class 1/src/variablesTwo.java
@@ -4,14 +4,14 @@
  github: Owen-3921
  Description: Prints out ten different lines
  Version: 1.0
- Last Updates: 10Sep2019
+ Last Updates: 12Sep2019
  */
 public class variablesTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int age = 18; 
+		int age = 18; //this is a variable for my age 
 		char middleInitial = 'P';
 		String myName = "Owen";
 		System.out.println("Hello, my name is " + myName +  " " + middleInitial  );
