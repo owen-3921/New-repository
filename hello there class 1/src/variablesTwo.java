206cf983fcb52478b4b20e54a22db317fa1df24d
oshoemaker <oshoemaker@10.242.5.110>
2019-09-12 23:49:24 -0400

    final project

---
+++ hello there class 1/src/variablesTwo.java
@@ -0,0 +1,21 @@
+/* 
+ Project: Ten lines
+ Author: Owen Shoemaker
+ github: Owen-3921
+ Description: Prints out ten different lines
+ Version: 1.0
+ Last Updates: 10Sep2019
+ */
+public class variablesTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 18; 
+		char middleInitial = 'P';
+		String myName = "Owen";
+		System.out.println("Hello, my name is " + myName +  " " + middleInitial  );
+        System.out.print("My age is " + age );
+	}
+
+}
