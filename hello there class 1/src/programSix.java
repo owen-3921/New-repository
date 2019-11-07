191c60cd5ba42bc1742fb78cab094727e2e2d358
oshoemaker <oshoemaker@10.242.5.110>
2019-11-07 11:30:07 -0500

    resubmitted

--- hello there class 1/src/programSix.java
+++ hello there class 1/src/programSix.java
@@ -1,9 +1,74 @@
+import java.util.Scanner;
 
 public class programSix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner keyboard = new Scanner(System.in);
+		int menuChoice;
+	
+		
+		
+		
+		account accOne = new account();
+		
+		do
+		{
+		displayMenu();
+		menuChoice = keyboard.nextInt();
+		
+		switch(menuChoice)
+		{
+		case 1:
+			System.out.println("How much money would you like to deposit?");
+			accOne.depositAmount = keyboard.nextDouble();
+			accOne.accountBalance = account.deposit(accOne.accountBalance, accOne.depositAmount);
+			accOne.displayBalance(accOne.accountBalance);
+			break;
+		case 2:
+			System.out.println("How much money would you like to withdraw?");
+			accOne.withdrawalAmount = keyboard.nextDouble();
+			accOne.accountBalance = account.withdrawal(accOne.accountBalance, accOne.withdrawalAmount);
+			accOne.displayBalance(accOne.accountBalance);
+			break;
+		case 3:
+			accOne.displayBalance(accOne.accountBalance);
+			break;
+		case 4:
+			accOne.displayBalance(accOne.accountBalance);
+			System.out.println("Thank you,  have a wonderful day!");
+			break;
+		default:
+			System.out.println("That is an invalid choice. Please chosse a valid option.");
+		}
+		
+		
+		
+		}while(menuChoice !=4);
+		
+		
+		
+	}
+
+	
+	
+	
+		
+		
+	
+
+
+	public static void displayMenu()
+	{
+		System.out.println("Main Menu");
+		System.out.println("=========");
+		System.out.println("1.) Make Deposit");
+		System.out.println("2.) Withdrawal");
+		System.out.println("3.) View Balance");
+		System.out.println("4.) Exit");
+		System.out.println("What would you like to do? ");
+		
 	}
 
 }
